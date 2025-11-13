7be0fdd6a26e8437814ccc18dd46df8ec03ccf94
abhishek1411-ai <abhishekdevmurari1411@gmail.com>
2025-11-13 15:13:51 +0530

    index

---
+++ codepules/src/componots/Layout.jsx/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from 'a componets/logo.jpg';
+
+
+export default function Header() {
+    return (
+        <header className="header">
+            <div className="header-content">
+                <img src="your-image.png" alt="logo" className="logo" />
+                <h1>CodePulse</h1>
+                <nav>
+                    
+                </nav>
+            </div>
+        </header>
+    );
+}
