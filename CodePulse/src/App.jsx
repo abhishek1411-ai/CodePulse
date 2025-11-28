64e5e9c241e6336468a1f12072f3f7f9a2d76e8d
Abhishek-1411 <abhishekdevmurari1411@gmail.com>
2025-11-28 21:49:18 +0530

    new

--- CodePulse/src/App.jsx
+++ CodePulse/src/App.jsx
@@ -29,7 +29,7 @@ export default function App() {
 
       <ToastContainer/>
 
-      <BrowserRouter>
+      <BrowserRouter basename="/CodePulse">
         <Header/>
         <Routes>
           <Route path="/" element={<Home/>}/>
